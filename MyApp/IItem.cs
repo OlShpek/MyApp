cc0cccb101d0e5fc188f40d096f7540473c8bb7e
OlShpek <ashpek1007@gmail.com>
2024-07-07 18:28:12 +0100

    First commit trying to change net version

---
+++ MyApp/IItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MyApp
+{
+    internal abstract class IItem
+    {
+        protected long id;
+
+        protected IItem()
+        {
+            set_id();
+        }
+        protected void set_id()
+        {
+            TimeSpan sec = DateTime.Now.Subtract(new DateTime(2024, 7, 7));
+            //potential error of repeating ids if created to fast
+            id = (long)sec.TotalSeconds;
+        }
+        public abstract XmlElement getXml();
+        
+    }
+}
---
+++ MyApp/Task.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MyApp
+{
+    internal class Task : IItem
+    {
+        private string content;
+        private DateTime dueDate, expTime;
+        public Task(string content, DateTime dueDate, DateTime expTime) : base()
+        {
+            this.content = content;
+            this.dueDate = dueDate;
+            this.expTime = expTime;
+        }
+
+        public bool is_overdue()
+        {
+            return DateTime.Now < dueDate;
+        }
+
+        public override XmlElement getXml()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement el = doc.CreateElement("task");
+            el.SetAttribute("id", id.ToString());
+            el.SetAttribute("content", content);
+            el.SetAttribute("dueDate", dueDate.ToString());
+            el.SetAttribute("expTime", expTime.ToString());
+            return el;
+        }
+
+        public string Content { get { return content; }  set { content = value; } }
+        public DateTime DueDate { get { return dueDate; } set { dueDate = value; } }
+        public DateTime ExpTime { get { return expTime; } set { expTime = value; } }
+        public long Id { get { return id; } }
+    }
+}
